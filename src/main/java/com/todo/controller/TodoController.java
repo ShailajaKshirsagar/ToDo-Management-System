9ceffc46812fece5463d9755c7a9bf7ee63fac9d
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2026-03-08 15:34:22 +0530

    API Implemented

--- src/main/java/com/todo/controller/TodoController.java
+++ src/main/java/com/todo/controller/TodoController.java
@@ -5,10 +5,10 @@ import com.todo.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/todos")
@@ -18,9 +18,30 @@ public class TodoController {
     TodoService todoService;
 
     // Create Task
-    @PostMapping
+    @PostMapping("/createTodo")
     public ResponseEntity<String> createTodo(@RequestBody Todo todo) {
         String msg = todoService.createTodo(todo);
         return new ResponseEntity<>(msg, HttpStatus.CREATED);
+    }
+
+    //get all tasks
+    @GetMapping("/getAllList")
+    public  ResponseEntity<List<Todo>> getAllTodoList(){
+        List<Todo> todolist = todoService.getAllList();
+        return new ResponseEntity<>(todolist,HttpStatus.OK);
+    }
+
+    //get by id
+    @GetMapping("/getById/{id}")
+    public ResponseEntity<Optional<Todo>> getById(@PathVariable("id") long id){
+        Optional<Todo> todoById = todoService.getById(id);
+        return new ResponseEntity<>(todoById,HttpStatus.OK);
+    }
+
+    //delete by id
+    @DeleteMapping("/deleteById/{id}")
+    public ResponseEntity<String> deleteTodo(@PathVariable("id") long id){
+        String msg = todoService.deleteById(id);
+        return new ResponseEntity<>(msg,HttpStatus.OK);
     }
 }
--- src/main/java/com/todo/service/TodoService.java
+++ src/main/java/com/todo/service/TodoService.java
@@ -5,6 +5,9 @@ import com.todo.repository.TodoRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class TodoService {
 
@@ -14,5 +17,19 @@ public class TodoService {
     public String createTodo(Todo todo) {
         todoRepository.save(todo);
         return "Task Saved";
+    }
+
+    public List<Todo> getAllList() {
+        return todoRepository.findAll();
+    }
+
+    public Optional<Todo> getById(Long id) {
+        Optional<Todo> byid = todoRepository.findById(id);
+        return byid;
+    }
+
+    public String deleteById(Long id) {
+        todoRepository.deleteById(id);
+        return "Todo Deleted";
     }
 }
